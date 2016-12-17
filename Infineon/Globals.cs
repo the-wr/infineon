ec5b696055b669a8b954d76eada3f5793d337784
Sergey Troshin <wr.main@gmail.com>
2016-12-17 18:45:45 +0100

    App skeleton

---
+++ Infineon/Globals.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media;
+
+namespace Infineon
+{
+    public class Globals
+    {
+        public static Brush HighlightBrush = new SolidColorBrush(Color.FromArgb( 32, 254, 189, 34 ) );
+    }
+}
