bc814131abd6492f48c057f3a9916c6fbc4fd80a
Sergey Troshin <wr.main@gmail.com>
2016-12-17 19:19:30 +0100

    SliderWithNumber

--- Infineon/App.xaml.cs
+++ Infineon/App.xaml.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Windows;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace Infineon
 {
@@ -12,5 +10,44 @@ namespace Infineon
     /// </summary>
     public partial class App: Application
     {
+        // http://stackoverflow.com/questions/660554/how-to-automatically-select-all-text-on-focus-in-wpf-textbox
+        protected override void OnStartup( StartupEventArgs e )
+        {
+            // Select the text in a TextBox when it receives focus.
+            EventManager.RegisterClassHandler( typeof( TextBox ), TextBox.PreviewMouseLeftButtonDownEvent,
+                new MouseButtonEventHandler( SelectivelyIgnoreMouseButton ) );
+            EventManager.RegisterClassHandler( typeof( TextBox ), TextBox.GotKeyboardFocusEvent,
+                new RoutedEventHandler( SelectAllText ) );
+            EventManager.RegisterClassHandler( typeof( TextBox ), TextBox.MouseDoubleClickEvent,
+                new RoutedEventHandler( SelectAllText ) );
+            base.OnStartup( e );
+        }
+
+        void SelectivelyIgnoreMouseButton( object sender, MouseButtonEventArgs e )
+        {
+            // Find the TextBox
+            DependencyObject parent = e.OriginalSource as UIElement;
+            while ( parent != null && !( parent is TextBox ) )
+                parent = VisualTreeHelper.GetParent( parent );
+
+            if ( parent != null )
+            {
+                var textBox = (TextBox)parent;
+                if ( !textBox.IsKeyboardFocusWithin )
+                {
+                    // If the text box is not yet focused, give it the focus and
+                    // stop further processing of this click event.
+                    textBox.Focus();
+                    e.Handled = true;
+                }
+            }
+        }
+
+        void SelectAllText( object sender, RoutedEventArgs e )
+        {
+            var textBox = e.OriginalSource as TextBox;
+            if ( textBox != null )
+                textBox.SelectAll();
+        }
     }
 }
--- Infineon/Controls/SliderWithNumber.xaml.cs
+++ Infineon/Controls/SliderWithNumber.xaml.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Infineon.Controls
 {
@@ -19,12 +11,67 @@ namespace Infineon.Controls
     /// </summary>
     public partial class SliderWithNumber: UserControl
     {
+        private double multiplier = 1;
+        private bool muted;
+
         public SliderWithNumber()
         {
             InitializeComponent();
 
             MouseEnter += delegate { rectHighlight.Fill = Globals.HighlightBrush; };
             MouseLeave += delegate { rectHighlight.Fill = Brushes.Transparent; };
+
+            slValue.ValueChanged += OnSliderValueChanged;
+            tbValue.TextChanged += OnTextChanged;
+            tbValue.LostFocus += OnLostFocus;
+
+            Setup( "Unnamed", 0, 10, 1 );
+        }
+
+        public void Setup( string name, int min, int max, double multiplier )
+        {
+            tbName.Text = name;
+            slValue.Minimum = min;
+            slValue.Maximum = max;
+            slValue.IsSnapToTickEnabled = true;
+
+            this.multiplier = multiplier;
+        }
+
+        private void OnSliderValueChanged( object sender, RoutedPropertyChangedEventArgs<double> routedPropertyChangedEventArgs )
+        {
+            if ( muted )
+                return;
+
+            muted = true;
+
+            tbValue.Text = $"{slValue.Value * multiplier:0.0}";
+
+            muted = false;
+        }
+
+        private void OnTextChanged( object sender, TextChangedEventArgs e )
+        {
+            if ( muted )
+                return;
+
+            muted = true;
+
+            double d;
+            if ( double.TryParse( tbValue.Text, NumberStyles.Any, CultureInfo.CurrentCulture, out d ) )
+                slValue.Value = (int)Math.Round( d / multiplier );
+
+            muted = false;
+        }
+
+        private void OnLostFocus( object sender, RoutedEventArgs e )
+        {
+            double d;
+            if ( double.TryParse( tbValue.Text, NumberStyles.Any, CultureInfo.CurrentCulture, out d ) )
+            {
+                slValue.Value = (int) Math.Round( d / multiplier );
+                tbValue.Text = $"{slValue.Value * multiplier:0.0}";
+            }
         }
     }
 }
